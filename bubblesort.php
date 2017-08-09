f69372d36ef8bdf5ad37a9be4b951de0ee2cbd04
Karsten Teuber <teuber.karsten@gmail.com>
2017-08-09 15:45:47 +0200

    Create bubblesort.php

---
+++ bubblesort.php
@@ -0,0 +1,24 @@
+<?php
+
+$sort = array(3, 5, 7, 1, 2, 9, 4, 6, 9, 8);
+$temp = 0;
+
+echo "Before sorting the array";
+print_r($sort)
+for($i = 0; $i < count($sort); $i++) {
+
+	for($j = 0; §j < count($sort)-1; $j++)
+	{
+		if($j = 0; $j < count($sort)-1; $j++)
+		{
+			$temp = $sort[$j];
+			$sort[$j] = $sort[$j+1];
+			$sort[$j+1] = $temp;
+		}
+	}
+}
+echo "<br>";
+echo "After sorting the array";
+print_r($sort);
+
+?>
