b6c084a90cee90996ba687d1c6a5a9ec5d60fcd2
Karsten Teuber <teuber.karsten@gmail.com>
2017-08-09 15:50:58 +0200

    Create hilfsliste.php

---
+++ hilfsliste.php
@@ -0,0 +1,19 @@
+function selectionSort($array)
+{
+    for ($i=0; $i<count($array); $i++)
+    {
+        // Position des kleinsten Elements suchen
+        $minpos=$i;
+        for ($j=$i+1; $j<count($array); $j++)
+            if ($array[$j]<$array[$minpos])
+                $minpos=$j;
+        // Elemente vertauschen
+        $tmp=$array[$minpos];
+        $array[$minpos]=$array[$i];
+        $array[$i]=$tmp;
+    }
+    return $array;
+}
+
+//Zur Kontrolle
+print_r(selectionSort(array('F', 'A', 'B', 'E', 'D', 'C', 'H', 'G')));
