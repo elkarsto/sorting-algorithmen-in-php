f87875f38a3c903f4dea90138bbc88371eebbe99
Karsten Teuber <teuber.karsten@gmail.com>
2017-08-29 15:37:05 +0200

    Update mergesort.php

--- mergesort.php
+++ mergesort.php
@@ -69,3 +69,103 @@ function print_array($array){
 }
 
 ?>
+
+//Das ist die effektivere Variante!---------------------------------------------------------------------------------------------
+
+<?php
+@ini_set('display_errors', true);
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mergesort</title>
+    </head>
+    <body>
+        <h1>Mergesort Algorithmus in PHP</h1>
+
+        <form>
+            <a href="index.php"><button type="button" id="Sortieren">Startseite</button></a>
+        </form><br>
+
+        <?php
+
+        function rand_num() {
+            return rand(1, 1000);
+        }
+
+        for ($i = 0; $i < 1000000; $i++) {
+            $number[$i] = rand_num();
+        }
+        //echo "Ausgabe des unsortierten Arrays:";
+        echo '<pre>';
+        //print_r($number);
+        echo '</pre>';        
+
+        function MergeSort($array) {
+            //echo "-------------------------------";
+            //print_r($array);
+
+            $anz = count($array);
+            if ($anz <= 1){ 
+                return $array;
+            }
+           
+            $hälfte = round($anz / 2);
+            $links = array_splice($array, 0, $hälfte);            
+            $rechts = $array;
+            //print_r($links);
+            //print_r($rechts);
+            $sortedlinks = MergeSort($links);
+            $sortedrechts = MergeSort($rechts);
+
+            // Merge
+            $array = [];
+            do {
+                if ($sortedlinks[0] <= $sortedrechts[0]){
+                    $array[] = array_shift($sortedlinks);
+                } else {
+                    $array[] = array_shift($sortedrechts);
+                }
+                
+            } while (count($sortedlinks) > 0 && count($sortedrechts) > 0);
+            
+            $array = array_merge($array, $sortedlinks, $sortedrechts);
+            //print_r($array);
+            return $array;
+        }
+                 
+           
+            
+            
+            
+        
+        echo '<pre>';
+        $start = microtime(true);
+        sort($number);
+        //print_r($sortiert);
+        $time_elapsed_secs = microtime(true) - $start;
+        echo $time_elapsed_secs;
+        echo '</pre>';
+
+        echo '<pre>';
+        $start = microtime(true);
+        sort(array_reverse($number));
+        //print_r($sortiert);
+        $time_elapsed_secs = microtime(true) - $start;
+        echo $time_elapsed_secs;
+        echo '</pre>';
+
+        echo '<pre>';
+        $start = microtime(true);
+        sort($number);
+        //print_r($sortiert);
+        $time_elapsed_secs = microtime(true) - $start;
+        echo $time_elapsed_secs;
+        echo '</pre>';
+        ?>
+
+
+    </body>
+</html>
+?>
